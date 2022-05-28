af9a2061c5e05ca67a23a4f88c4271e09fce3e5f
MohaDarkness <Mohannad.Atmeh@gmail.com>
2022-05-29 00:59:44 +0300

    Asignment 2.3

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect("mongodb+srv://MohaDarkness:MohaDarknessPSUT@cluster0.ruwoqvj.mongodb.net/?retryWrites=true&w=majority", 
+    ()=>{
+        console.log("We are connected to the database ^_^");
+    }, 
+    ()=>{
+        console.log("There's an error somewhere!");
+    }
+);
 
 const body_parser = require('body-parser');
 app.use(body_parser.urlencoded({'extended':false}));
--- routes/movies.js
+++ routes/movies.js
@@ -1,49 +1,48 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var path = require('path');
+const { stringify } = require('querystring');
 var router = express.Router();
 
 router.use(express.urlencoded({ extended: true }));
 
+const movieSchema = new mongoose.Schema({
+    title:  {type:String, require:true},
+    release:{type:Number, require:true},
+    genre:  {type:String, require:true}
+}, {collection: 'movies-data'});
 
-let movies = [
-    {title:"The Batman", release:"2022", genre:"Action"},
-    {title:"I Am Sam", release:"2020", genre:"Drama"}
-];
+var MoviesData = mongoose.model('MoviesData', movieSchema);
 
-movies = movies.map( (v, ind) => {
-    return {
-        ...v,
-        id : ind
-    }
-} );
 
 router.get('/movies/add', (req, res, next)=>{
-    res.render('addMovies', {movies,});
+    var movies = MoviesData.find({},(err, movies)=>{
+        res.render('addMovies', movies);
+    });
 });
 
 router.get('/movies', (req, res, next)=>{
-    res.render('movies',{movies,});
+    var movies = MoviesData.find({},(err, movies)=>{
+        console.log(movies);
+        res.render('movies', {movies});
+    });
 });
 
 
 router.post('/movies', (req, res, next)=>{
-    const movieToAdd = {
+    MoviesData.create({
         title: req.body.mName,
         release: req.body.mRelease,
         genre: req.body.mGenre,
-        id: movies.length
-    }
-    movies.push(movieToAdd);
-    console.log(movies);
+    });
+
     res.redirect("/movies");
 });
 
 router.get("/movies/:id", (req, res, next) => {
-    const movie = movies.find((ite) => ite.id === Number(req.params.id));
-    console.log(movie);
-    
-    // console.log(req.params.id);
-    res.render("movieDetails", { title : movie.title, release: movie.release, genre: movie.genre });
+    MoviesData.findById(req.params.id,(err, movie)=>{
+        res.render("movieDetails", { title : movie.title, release: movie.release, genre: movie.genre });
+    });
 });
 
 
